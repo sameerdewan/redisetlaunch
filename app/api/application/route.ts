c368f7da7e8eec749a01a190e5f2519fbb785294
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-15 20:41:35 -0400

    applications route serverless fn

--- app/api/application/route.ts
+++ app/api/application/route.ts
@@ -4,21 +4,52 @@ import applicationRepositoryClient from "@/repositories/ApplicationRepository"
 
 export async function GET(req: Request) {
     try {
-        const subscriptionKey = req.headers.get('x-flags-subscription-key');
-        let userId: string | null = null;
-        if (subscriptionKey === null) {
-            userId = auth().userId;
-            if (userId === null) return new NextResponse('Unauthorized', {status: 401});
-        }
-        // TODO: Validate the subscription key
-        // TODO: Populate userId from the fetched subscription
-        userId = ''
+        const userId = auth().userId;
+        if (userId === null) return new NextResponse('Unauthorized', {status: 401});
         const body = await req.json();
-        const application = await applicationRepositoryClient.getApplicationByIdAndUserId(
-            body.id,
-            userId
+        if (body.id) {
+            const application = await applicationRepositoryClient.getApplicationByIdAndUserId(
+                body.id,
+                userId
+            );
+            return NextResponse.json(application);
+        }
+        const applications = await applicationRepositoryClient.getApplicationsByUserId(userId);
+        return NextResponse.json(applications);
+    } catch (error) {
+        console.log(
+            'Application error',
+            error,
+            JSON.stringify({error}, null, 2)
         );
-        return NextResponse.json(application);
+        return new NextResponse('Internal error', {status: 500});
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const userId = auth().userId;
+        if (userId === null) return new NextResponse('Unauthorized', {status: 401});
+        const application = await req.json();
+        const savedApplication = await applicationRepositoryClient.saveApplication(application);
+        return NextResponse.json(savedApplication);
+    } catch (error) {
+        console.log(
+            'Application error',
+            error,
+            JSON.stringify({error}, null, 2)
+        );
+        return new NextResponse('Internal error', {status: 500});
+    }
+}
+
+export async function PUT(req: Request) {
+    try {
+        const userId = auth().userId;
+        if (userId === null) return new NextResponse('Unauthorized', {status: 401});
+        const application = await req.json();
+        const savedApplication = await applicationRepositoryClient.saveApplication(application);
+        return NextResponse.json(savedApplication);
     } catch (error) {
         console.log(
             'Application error',
