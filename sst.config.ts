f01bdc4339f5d0303d5dcaae51bf6a06b2f8d5f0
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-29 17:52:32 -0400

    run db

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,14 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          AWS_REGION: process.env.AWS_REGION!,
+          AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+          AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+          DYNAMO_ENDPOINT: process.env.DYANMO_ENDPOINT!
+        }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
