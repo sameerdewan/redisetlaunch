d4ec7b4726095af97e03b5ad2c629006d21be37e
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-28 23:44:33 -0400

    pin packages and remove issue with radix dialog 1.0.5

--- app/api/v1/auth/login/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server";
-import {createNextRequest, NextApiReq} from "@/lib/utils";
-
-export const PUT = async (req: NextApiReq) => createNextRequest(
-    "PUT /api/v2/login",
-    async () => {
-        return NextResponse.json([]);
-    }
-);
--- app/api/v1/auth/register/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server";
-import {createNextRequest, NextApiReq} from "@/lib/utils";
-
-export const POST = async (req: NextApiReq) => createNextRequest(
-    "POST /api/v2/register",
-    async () => {
-        return NextResponse.json([]);
-    }
-);
